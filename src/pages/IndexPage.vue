e4941d2bc9792a66149388a4fb21da7a25be841c
Harvmaster <hzuccon@gmail.com>
2024-03-18 23:45:45 +1100

    Experimenting with provider elements

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,7 +7,7 @@
       <div class="col-12 row page-content title-container justify-center">
 
         <!-- Title -->
-        <div class="col-auto q-py-xl title-header">
+        <div class="col-auto q-py-xl title-header q-px-md">
           <div class="col-12">
             <h1 class="text-h1 text-weight-bolder text-center no-margin q-pb-md page-header">Welcome to Groceries</h1>
           </div>
@@ -18,7 +18,7 @@
         </div>
 
         <!-- Search -->
-        <div class="col-12 row justify-center">
+        <div class="col-12 row justify-center q-px-md">
           <q-input class="col search-bar" color="primary" bg-color="white" outlined v-model="search" label="Search for groceries" />
         </div>
       </div>
@@ -51,9 +51,72 @@
     </div>
 
     <!-- Scrapers -->
-    <div class="col-12 row justify-center">
+    <div class="col-12 row justify-center bg-27">
       <div class="col-12 col-md-8 row justify-center q-pa-xl">
         
+        <!-- Coles -->
+        <div class="col-12 col-md-4 col-sm-6 row provider-card">
+          <div class="provider-logo" />
+          <div class="col-12 row items-end justify-center provider-content-container q-pa-md">
+            <div class="col-12 row q-col-gutter-y-sm provider-content fit  text-white text-weight-bold q-pa-xs">
+              <div class="col-12 column">
+                <div class="col-auto height-auto">
+                  <div class="col-auto q-pa-sm text-h3 text-weight-bolder">
+                    Coles
+                  </div>
+                </div>
+
+                <div class="col-auto row self-center justify-center height-auto q-pa-sm">
+                  <div class="col-4 text-center row justify-center stat">
+                    <p class="col-auto text-h5 no-margin">+10%</p>
+                    <p class="col-12 text-h5 no-margin">Comparative Prices</p>
+                  </div>
+                </div>
+                
+                <div class="col-auto row justify-evenly height-auto q-pa-sm">
+                  <div class="col-12 row justify-evenly height-auto q-pa-sm">
+                    <div class="col-4 text-center row justify-center stat">
+                      <p class="col-auto text-h5 no-margin">-10%</p>
+                      <p class="col-12 text-h5 no-margin">Store Prices</p>
+                    </div>
+                    <div class="col-4 text-center row justify-center stat">
+                      <p class="col-auto text-h5 no-margin">-10%</p>
+                      <p class="col-12 text-h5 no-margin">Store Prices</p>
+                    </div>  
+                  </div>
+
+                  <!-- <div>
+                    <p class="text-h5 no-margin">Comparative Prices: <span class="text-red-1 text-weight-bolder">+10%</span></p>
+                    <p class="text-h5 no-margin">Store Prices: <span class="text-green-4 text-weight-bolder">-10%</span></p>
+                    <p class="text-h5 no-margin">Sales: <span class="text-red-1 text-weight-bolder">-10%</span></p>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="col-4 row provider-card">
+          <div class="provider-logo" />
+          <div class="col-12 row items-end justify-center provider-content-container q-pa-md">
+            <div class="col-12 row q-col-gutter-y-sm provider-content text-weight-bold q-pa-xs">
+              <div class="col-auto">
+                <div class="col-auto q-pa-sm text-h5 text-weight-bolder">
+                  Coles
+                </div>
+              </div>
+              <div class="col-12">
+                <div>
+                  <p class="text-h6 no
+                  -margin">Comparative Prices: <span class="text-red-1 text-weight-bolder">+10%</span></p>
+                  <p class="text-h6">Store Prices: <span class="text-green-4 text-weight-bolder">-10%</span></p>
+                  <p class="text-h6">Sales: <span class="text-red-1 text-weight-bolder">-10%</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
       </div>
     </div>
 
@@ -118,7 +181,8 @@
   background-size: cover;
   background-position: center;
   z-index: -1;
-  // transform: translateY(-50%);
+  // transform: translateY;
+  filter: blur(8px);
 }
 
 .search-bar {
@@ -140,15 +204,17 @@
   //   height: 100%;
   // }
 }
-.provider-content {
-  box-shadow: 4px 4px 4px 8px rgba(0, 0, 0, 0.212);
-  border-radius: 1em;
-}
+// .provider-content {
+//   box-shadow: 4px 4px 4px 8px rgba(0, 0, 0, 0.212);
+//   border-radius: 1em;
+// }
 .landing-section {
   // https://ideogram.ai/api/images/direct/7O2w5iKUSsO1zgy1lElUMw.png
   // https://ideogram.ai/api/images/direct/8NbQxLISRCSNW3mP_o7Wkg.jpg
   // https://ideogram.ai/api/images/direct/1UhTIWYeQkCXY6eXG2PyXg.jpg
   background-color: rgba(0, 0, 0, 0.3);
+  // backdrop-filter: blur(20px);
+
 
   // background-image: url('https://ideogram.ai/api/images/direct/8NbQxLISRCSNW3mP_o7Wkg.jpg');
   // background-size: cover;
@@ -163,6 +229,9 @@
 .page-header {
   text-shadow: 1px 1px 4px rgb(0, 0, 0);
 }
+.text-h1 {
+  font-size: min(6rem, 16vw);
+}
 .title-header {
   // background-color: rgba(255, 255, 255, 0.8);
   // border-radius: 1em;
@@ -172,6 +241,117 @@
 
 }
 .stats-bar {
+  background-color: #272727;
+}
+
+// .provider-card {
+//   position: relative;
+//   aspect-ratio: 1;
+//   height: auto;
+//   border-radius: 1em;
+//   overflow: hidden;
+//   box-shadow: 2px 2px 8px 1px rgba(0, 0, 0, 0.4);
+// }
+
+// .provider-logo {
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   // object-fit: cover;
+//   height: auto;
+//   aspect-ratio: 16/9;
+//   // height: 100%;
+//   background-image: url('https://logos-world.net/wp-content/uploads/2022/04/Coles-Symbol.png');
+//   background-size: contain;
+//   background-position: center;
+//   background-repeat: repeat;
+//   z-index: 0;
+// }
+// .provider-content-container {
+  
+// }
+// .provider-content {
+//   height: min-content;
+//   padding: 0.2em;
+//   // padding: 0.5em 1em;
+//   background-color: rgb(255, 255, 255);
+//   // background-color: rgb(236, 24, 24); // Color match to coles
+//   // border-radius: 2em;
+//   // box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.7);
+
+//   color: #272727;
+//   font-weight: bolder;
+//   backdrop-filter: blur(8px);
+//   // backdrop-filter: blur(8px) brightness(1) sepia(0.5);
+//   border-radius: 0.8rem;
+//   // text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.199);
+//   // outline: 1px solid white;
+
+//   div {
+//     padding: 0.1em;
+//     div {
+//       border-radius: 0.8rem;
+//       background-color: rgb(245, 245, 245);
+//     }
+//   }
+// }
+
+.provider-card {
+  position: relative;
+  aspect-ratio: 1;
+  border-radius: 1em;
+  overflow: hidden;
+  box-shadow: 2px 2px 8px 1px rgba(0, 0, 0, 0.4);
+}
+
+.provider-logo {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('https://logos-world.net/wp-content/uploads/2022/04/Coles-Symbol.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: repeat;
+  z-index: 0;
+  filter: blur(4px);
+}
+.provider-content-container {
+  
+}
+.provider-content {
+  height: min-content;
+  padding: 0.2em;
+  // padding: 0.5em 1em;
+  // background-color: rgb(255, 255, 255);
+  // background-color: rgb(236, 24, 24); // Color match to coles
+  // border-radius: 2em;
+  // box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.7);
+
+  color: #272727;
+  font-weight: bolder;
+  backdrop-filter: blur(8px) brightness(0.8);
+  // backdrop-filter: blur(8px) brightness(1) sepia(0.5);
+  border-radius: 0.8rem;
+  // text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.199);
+  // outline: 1px solid white;
+
+  // div {
+  //   padding: 0.1em;
+  //   div {
+  //     border-radius: 0.8rem;
+  //     background-color: rgb(245, 245, 245);
+  //   }
+  // }
+}
+
+.height-auto {
+  height: auto;
+}
+
+.bg-27 {
   background-color: #272727;
 }
 </style>
