bba1172aa5e6c997234b6ae7cbe58baa0a6cab71
Harvmaster <hzuccon@gmail.com>
2024-06-18 02:06:33 +1000

    Added Product to type file

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  retailer_product_id: string;
+  retailer_product_url: string;
+  retailer_name: string;
+  name: string;
+  barcode: string;
+  img_url: string;
+  tags: string[];
+  create_date: string;
+}
